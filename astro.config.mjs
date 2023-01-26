210f2c38d3977de11156a774497d27ad009906b8
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-01-26 14:49:07 -0600

    setting up storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,19 @@ import netlify from "@astrojs/netlify/functions";
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [storyblok({
-    accessToken: "LSTMzttTLabrQLn9YBzQowtt",
-    bridge: true,
-    apiOptions: {
-      region: "us"
-    },
-    components: {
-      page: "storyblok/Page",
-      feature: "storyblok/Feature",
-      grid: "storyblok/Grid",
-      teaser: "storyblok/Teaser"
-    }
-  })]
+  integrations: [
+    storyblok({
+      accessToken: "sGbxifnNJ2K63cMQPKbGSgtt",
+      bridge: true,
+      apiOptions: {
+        region: "us",
+      },
+      components: {
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+      },
+    }),
+  ],
 });
